d52db9723b75bc52a3f52db29515362196f9d8d6
nowhard <nowhard@mail.ru>
2015-04-20 16:03:48 +0300

    Добавлен лог порт

--- memtest/uUSO_MASTER/debug.c
+++ memtest/uUSO_MASTER/debug.c
@@ -5,6 +5,10 @@
 
 volatile unsigned char xdata mem1 _at_ 0x5555;
 volatile unsigned char xdata mem2 _at_ 0x2AAA;
+
+volatile unsigned char xdata log_port_in_1  _at_ 0x8001;
+volatile unsigned char xdata log_port_out_1 _at_ 0x8002;
+volatile unsigned char xdata log_port_out_2 _at_ 0x8003;
 //-----------------------------------------
 #define PLLCON_VAL 0xF8
  //---------------------------------------
@@ -17,13 +21,16 @@ void main(void) //using 0
 	
 	EA=1;
 
-
+//	log_port_out_1=MEM1_CONST;
+//	log_port_out_2=MEM2_CONST;
 
 	while(1)
 	{
-		mem1=MEM1_CONST;
-		mem2=MEM2_CONST;
-		(void)mem1;
-		(void)mem2;		 
+//		mem1=MEM1_CONST;
+//		mem2=MEM2_CONST;
+//		(void)mem1;
+//		(void)mem2;	
+//	log_port_out_1=MEM1_CONST;
+	log_port_out_2=MEM2_CONST;	 
 	}
 }
